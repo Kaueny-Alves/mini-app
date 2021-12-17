f5df0179acc6595c85180a7f6c277ee169361e91
Kaueny Alves <kauenyalves@hotmail.com>
2021-12-16 21:07:38 -0300

    Links de redirect nos botões da Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,20 +3,25 @@ import marcaImg from "../../assets/marca_mini_app.png"
 import responsiveImg from "../../assets/img_responsive.png"
 import { ButtonGreen, ButtonOrange } from '../../components/Button'
 import { Container, ContentImg, ContentBtn } from './styles'
+import { Link } from 'react-router-dom'
 
 export function Home() {
 
-  
+
   return (
     <Container >
       <ContentImg>
-      <img src={responsiveImg} alt='imagem de dispositivos eletronicos' className='responsiveImg' />
-      <img src={marcaImg} alt='imagem do logo da marca' className='marcaImg' />
+        <img src={responsiveImg} alt='imagem de dispositivos eletronicos' className='responsiveImg' />
+        <img src={marcaImg} alt='imagem do logo da marca' className='marcaImg' />
       </ContentImg>
       <ContentBtn>
-      <ButtonOrange/>
-      <ButtonGreen/>
+        <Link to={'/register'}>
+          <ButtonOrange />
+        </Link>
+        <Link to={'/login'}>
+          <ButtonGreen />
+        </Link>
       </ContentBtn>
     </Container>
   )
-  }
+}
