257a96c443e7894e7fd3d981aef58b282246e473
Kaueny Alves <kauenyalves@hotmail.com>
2021-12-19 23:46:28 -0300

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import { GlobalStyle } from './global';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Lists } from './pages/Lists';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { Home } from './pages/Home/Home';
+import { CreateLists } from './pages/Lists/CreateLists';
+
+
 
 export function App() {
+
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
-    </>
+      <Routes>
+        <Route path="/" element={<Home children={''} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/lists" element={<Lists  />} />
+        <Route path="/create" element={<CreateLists />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
