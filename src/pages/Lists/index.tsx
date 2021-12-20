ad27f5ef6bb3b5840adaa2ab0d4806352a6a6e20
Kaueny Alves <kauenyalves@hotmail.com>
2021-12-19 23:38:38 -0300

    Criação dos elementos da tela de lista

--- src/pages/Lists/index.tsx
+++ src/pages/Lists/index.tsx
@@ -1,11 +1,92 @@
-import React from 'react'
+import { TextField } from '@material-ui/core'
+import React, { ChangeEvent, useState } from 'react'
 import { Header } from '../../components/Header'
+import icone_lista from "../../assets/icone_lista.png";
+import adicionar from "../../assets/adicionar.png";
+import editar from "../../assets/icone_editar.png";
+import excluir_lista from "../../assets/icone_deletar_lista.png";
+import excluir_tarefa from "../../assets/icone_deletar_tarefa-subtarefa.png";
+import { Container, ContentTitle, ContainerList } from './styles';
+
+
 
 export function Lists() {
+
+  const [lists, setLists] = useState([{
+    id: "1",
+    nameList: "Lista 1",
+    tasks: [{
+      id: "1",
+      nameTask: "Tarefa 1",
+      subTarefa: [{
+        id: "1",
+        Subtarefa: "Subtarefa 1"
+      },
+      {
+        id: "2",
+        Subtarefa: "Subtarefa 2"
+      },
+      {
+        id: "3",
+        Subtarefa: "Subtarefa 3"
+      }
+      ]
+    }]
+  },
+  {
+    id: "2",
+    nameList: "Lista 2",
+    tasks: [{
+      id: "",
+      nameTask: "",
+      subTarefa: [{
+        id: "",
+        Subtarefa: ""
+      }]
+    }]
+  },
+  {
+    id: "3",
+    nameList: "Lista 3",
+    tasks: [{
+      id: "",
+      nameTask: "",
+      subTarefa: [{
+        id: "",
+        Subtarefa: ""
+      }]
+    }]
+  }])
+
   return (
-    <div>
-      <Header/>
-      
-    </div>
+    <>
+      <Header />
+      <Container>
+        <ContentTitle>
+          <h2 className="text">Listas</h2>
+          <button className="btn" >
+            <img src={adicionar} alt="imagem de adicionar" />
+          </button>
+        </ContentTitle>
+        {lists && lists.map((list) => {
+          return <ContainerList>
+            <div>
+              <button className="btn" >
+                <img src={icone_lista} alt="imagem de adicionar" />
+              </button>
+              {list.nameList}
+            </div>
+            <div>
+              <button className="btn" >
+                <img src={editar} alt="imagem de adicionar" />
+              </button>
+              <button className="btn" >
+                <img src={excluir_lista} alt="imagem de adicionar" />
+              </button>
+            </div>
+          </ContainerList>
+        })}
+      </Container>
+    </>
   )
 }
